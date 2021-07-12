087aa3d0a14d8cb7b62c721ed287ad6742eecc72
BXBots <86224222+BXBots@users.noreply.github.com>
2021-07-12 16:57:06 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -15,16 +15,16 @@ async def start(c, m):
         )
     
     await m.reply_text(
-        text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
+        text=f"Hi {m.from_user.first_name},\n\nIam Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌  Support Group', url='https://t.me/InFoTelGroup'),
-                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/TGBotsZ')
+                    InlineKeyboardButton('🤖 Update Channel', url='https://t.me/BX_Botz'),
+                    InlineKeyboardButton('👥Support Group', url='https://t.me/BxSupport')
                 ],
                 [
-                    InlineKeyboardButton('💡  Source Code', url='https://github.com/TGExplore/Screenshot-Bot'),
+                    InlineKeyboardButton('💡 ', url='https://github.com/TGExplore/Screenshot-Bot'),
                     InlineKeyboardButton('👨  Master', url='https://t.me/odbots')
                 ]
             ]
